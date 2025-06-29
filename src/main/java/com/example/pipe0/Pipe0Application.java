da33b07361a65a598af1610649c834ef629a4cdb
rajiv <bk.rajivgupta@gmail.com>
2025-06-29 12:40:52 +0530

    first commit

---
+++ src/main/java/com/example/pipe0/Pipe0Application.java
@@ -0,0 +1,13 @@
+package com.example.pipe0;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pipe0Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pipe0Application.class, args);
+	}
+
+}
---
+++ src/main/java/com/example/pipe0/controller/EmployeeController.java
@@ -0,0 +1,15 @@
+package com.example.pipe0.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/employees")
+public class EmployeeController {
+
+    @GetMapping
+    public String getEmployees() {
+        return "List of employees";
+    }
+}
